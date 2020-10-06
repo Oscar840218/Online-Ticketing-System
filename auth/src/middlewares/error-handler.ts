25710b16676487a7596f97e1eee85e1d69963ea4
RealDigit <oscar840218@gmail.com>
2020-10-06 16:36:14 -0700

    Add error handler

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { DatabaseConnectionError } from './../errors/database-connection-error';
+import { RequestValidationError } from './../errors/request-validation-error';
 
 export const errorHandler = (
     err: Error, 
@@ -6,6 +8,18 @@ export const errorHandler = (
     res: Response, 
     next: NextFunction
 ) => {
-    console.log('Something went wrong', err);
-    res.status(400).send({message: err.message});
+    if (err instanceof RequestValidationError) {
+        const formattedErros = err.errors.map(error => {
+            return { message: error.msg, feild: error.param };
+        });
+        return res.status(400).send({ erros: formattedErros });
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        return res.status(500).send({ errors: [
+            { message: err.reason }
+        ] });
+    }
+
+    res.status(400).send({message: 'Something went wrong'});
 };
