0006144097ade4fa1a3f175fe28401a93e26d06b
RealDigit <oscar840218@gmail.com>
2020-10-06 17:28:26 -0700

    add async

--- auth/src/routes/singup.ts
+++ auth/src/routes/singup.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid!'),
     body('password').trim().isLength({min: 4, max: 20}).withMessage('Length must between 4~20')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array());
