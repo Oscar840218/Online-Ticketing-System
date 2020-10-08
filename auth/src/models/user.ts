9196a1d2b4c73283e09f940316ec8ad5ab12d620
RealDigit <oscar840218@gmail.com>
2020-10-08 16:31:15 -0700

    add jwt authentication

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -27,6 +27,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.password;
+            delete ret._v;
+        }
+    }
 });
 
 // The middleware used before saving into database
--- auth/src/routes/singup.ts
+++ auth/src/routes/singup.ts
@@ -2,6 +2,7 @@ import { BadRequestErrors } from './../errors/bad-request-error';
 import { RequestValidationError } from './../errors/request-validation-error';
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/user'
 
 const router = express.Router();
@@ -28,6 +29,17 @@ router.post('/api/users/signup', [
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJWT = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+
+    // Store in session
+    req.session = {
+        jwt: userJWT
+    };
 
     res.status(201).send(user);
 });
