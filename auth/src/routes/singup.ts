1eb73e4425509a207d337db5a6ceb2e7a077fca4
RealDigit <oscar840218@gmail.com>
2020-10-08 17:23:38 -0700

    add signin

--- auth/src/routes/singup.ts
+++ auth/src/routes/singup.ts
@@ -1,7 +1,7 @@
+import { validateRequest } from './../middlewares/validate-request';
 import { BadRequestErrors } from './../errors/bad-request-error';
-import { RequestValidationError } from './../errors/request-validation-error';
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user'
 
@@ -10,11 +10,7 @@ const router = express.Router();
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid!'),
     body('password').trim().isLength({min: 4, max: 20}).withMessage('Length must between 4~20')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
+], validateRequest, async (req: Request, res: Response) => {
 
     console.log('Creating user...');
 
